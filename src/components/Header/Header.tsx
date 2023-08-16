90417e6ebd80b4bb7b1dc156e01529a549e6d612
Diana Vidal <dvidaal@gmail.com>
2023-08-16 19:05:48 +0200

    Create Header and styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = (): JSX.Element => {
+  return (
+    <header className="title">
+      <h1>List of Books</h1>
+    </header>
+  );
+};
+
+export default Header;
